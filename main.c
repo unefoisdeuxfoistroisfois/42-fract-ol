12a222c091aa34908f19e1297d742c3319d87c82
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-08-14 19:55:18 +0200

    Modification du nom de la mlx

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:55:19 by britela-          #+#    #+#             */
-/*   Updated: 2025/08/14 17:55:32 by britela-         ###   ########.fr       */
+/*   Updated: 2025/08/14 19:49:46 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int	main()
 	void	*win_ptr;// renovie l'identifiant de la nouvelle fentrre
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Bradley");//ouvrire une fentere
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "unefoisdeuxfoistroisfois");//ouvrire une fentere
 	mlx_loop(mlx_ptr);
 	
 	return (0);
