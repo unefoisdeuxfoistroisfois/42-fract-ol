c5a23da034de4e16fa8510763daf353211befbaf
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-08-14 17:57:15 +0200

    Amélioration du main pour pouvoir afficher la MLX

--- main.c
+++ main.c
@@ -6,11 +6,20 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:55:19 by britela-          #+#    #+#             */
-/*   Updated: 2025/08/13 16:55:38 by britela-         ###   ########.fr       */
+/*   Updated: 2025/08/14 17:55:32 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "mlx.h"
+
 int	main()
 {
+	void	*mlx_ptr;//car la mlx renvoie un void
+	void	*win_ptr;// renovie l'identifiant de la nouvelle fentrre
+
+	mlx_ptr = mlx_init();
+	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "Bradley");//ouvrire une fentere
+	mlx_loop(mlx_ptr);
+	
 	return (0);
 }
