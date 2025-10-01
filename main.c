29b31b2a6541dcb063f34280f43e909034d278f6
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-10-01 12:00:22 +0200

    Proctection pour mlx_init()

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/13 16:55:19 by britela-          #+#    #+#             */
-/*   Updated: 2025/10/01 10:15:50 by britela-         ###   ########.fr       */
+/*   Updated: 2025/10/01 11:37:42 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,11 @@ int	main()
 	void	*win_ptr;// renovie l'identifiant de la nouvelle fentrre
 
 	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 500, 500, "unefoisdeuxfoistroisfois");//ouvrire une fentere
+	if (mlx_ptr == NULL)
+	{
+			return (-1);
+	}
+	win_ptr = mlx_new_window(mlx_ptr, 800, 800, "unefoisdeuxfoistroisfois");//ouvrire une fentere
 	mlx_loop(mlx_ptr);
 	
 	return (0);
