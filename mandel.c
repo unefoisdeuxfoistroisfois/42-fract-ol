8720a4cb452d5978d302d81663d488467dfecfaa
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-11-02 17:01:48 +0100

    Implementation da la structure

--- mandel.c
+++ mandel.c
@@ -2,8 +2,9 @@
 
 void	mandelbrot(t_data *img, int width, int height, int max_iter)
 {
-	int		y;
-	int		x;
+	int				y;
+	int				x;
+	t_mandelbrot	m;
 
 	y = 0;
 	while (y < height)
@@ -11,28 +12,27 @@ void	mandelbrot(t_data *img, int width, int height, int max_iter)
 		x = 0;
 		while (x < width)
 		{
-			a = (x - width / 2.0) * 4.0 / width;
-			b = (y - height / 2.0) * 2.0 / height;
-
-			zx = 0.0;
-			zy = 0.0;
-			iter = 0;
-			while (zx * zx + zy * zy < 2 * 2 && iter < max_iter)
+			m.a = (x - width / 2.0) * 4.0 / width;
+			m.b = (y - height / 2.0) * 2.0 / height;
+			m.zx = 0.0;
+			m.zy = 0.0;
+			m.iter = 0;
+			while (m.zx * m.zx + m.zy * m.zy < 4.0 && m.iter < max_iter)
 			{
-				new_zx = (zx * zx - zy * zy) + a;  // x_{n+1}
-				new_zy = (2.0 * zx * zy) + b;      // y_{n+1}
-				zx = new_zx;
-				zy = new_zy;
-				iter++;
+				m.new_zx = (m.zx * m.zx - m.zy * m.zy) + m.a;
+				m.new_zy = (2.0 * m.zx * m.zy) + m.b;
+				m.zx = m.new_zx;
+				m.zy = m.new_zy;
+				m.iter++;
 			}
-			if (iter == max_iter)
-				color = 0xfa9f16;
+			if (m.iter == max_iter)
+				m.color = 0xfa9f16;
 			else
 			{
-				shade = (255 * iter) / max_iter;       // dégradé de gris
-				color = (shade << 16) | (shade << 8) | shade;
+				m.shade = (255 * m.iter) / max_iter;
+				m.color = (m.shade << 16) | (m.shade << 8) | m.shade;
 			}
-			my_mlx_pixel_put(img, x, y, color);
+			my_mlx_pixel_put(img, x, y, m.color);
 			x++;
 		}
 		y++;
