9d2c147c08fba2322cf6f3881ea724fbfea48627
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-11-03 21:26:23 +0100

    Erreur de syntax

--- mandel.c
+++ mandel.c
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 20:32:34 by britela-          #+#    #+#             */
-/*   Updated: 2025/11/03 20:38:19 by britela-         ###   ########.fr       */
+/*   Updated: 2025/11/03 20:51:47 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void	ft_init(t_mandelbrot *m, int x, int y)
 {
-	m->a = (x - WITDH / 2.0) * 4.0 / WITDH;
+	m->a = (x - WIDTH / 2.0) * 4.0 / WIDTH;
 	m->b = (y - HEIGHT / 2.0) * 2.0 / HEIGHT;
 	m->zx = 0.0;
 	m->zy = 0.0;
@@ -44,19 +44,19 @@ void	ft_color(t_mandelbrot *m, int max_iter)
 	}
 }
 
-void	mandelbrot(t_data *img, int WITDH, int HEIGHT, int max_iter)
+void	mandelbrot(t_data *img, int width, int height, int max_iter)
 {
 	int				y;
 	int				x;
 	t_mandelbrot	m;
 
 	y = 0;
-	while (y < HEIGHT)
+	while (y < height)
 	{
 		x = 0;
-		while (x < WITDH)
+		while (x < width)
 		{
-			ft_init(&m, x, y, WITDH, HEIGHT);
+			ft_init(&m, x, y);
 			ft_calcul(&m, max_iter);
 			ft_color(&m, max_iter);
 			my_mlx_pixel_put(img, x, y, m.color);
