8d89a1b5b6e16803ced2e74e580c9d76d196fcb2
unefoisdeuxfoistroisfois <1fois2fois3fois@gmail.com>
2025-11-08 16:13:54 +0100

    Implemation du zoom

--- event.c
+++ event.c
@@ -6,7 +6,7 @@
 /*   By: britela- <britela-@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/03 20:25:07 by britela-          #+#    #+#             */
-/*   Updated: 2025/11/08 11:56:28 by britela-         ###   ########.fr       */
+/*   Updated: 2025/11/08 16:10:16 by britela-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 int	ft_close_window(void *param)
 {
 	(void)param;
-	ft_printf("Vous avez quitté\n");
+	ft_printf ("Vous avez quitté\n");
 	exit(0);
 	return (0);
 }
@@ -66,14 +66,42 @@ int	ft_key(int key, void *param)
 	}
 	return (0);
 }
+
+void	ft_zoom(t_mouvement *move, double zoom)
+{
+	double	center_r;
+	double	center_i;
+
+	center_r = (move->r1 + move->r2) / 2.0;
+	center_i = (move->i1 + move->i2) / 2.0;
+	move->r1 = center_r + (move->r1 - center_r) * zoom;
+	move->r2 = center_r + (move->r2 - center_r) * zoom;
+	move->i1 = center_i + (move->i1 - center_i) * zoom;
+	move->i2 = center_i + (move->i2 - center_i) * zoom;
+}
+
 int	ft_mouse(int key_mouse, int x, int y, void *param)
 {
-	(void)param;
+	t_mouvement	*move;
+	double	zoom;
+
 	(void)x;
 	(void)y;
+	move = (t_mouvement *)param;
 	if (key_mouse == 4)
+	{
 		ft_printf("ZOOM +\n");
+		zoom = 0.9;
+	}
 	else if (key_mouse == 5)
+	{
 		ft_printf("ZOOM -\n");
+		zoom = 1.1;
+	}
+	else
+		return (0);
+	ft_zoom(move, zoom);
+	mandelbrot(&move->img, move);
+	mlx_put_image_to_window(move->mlx, move->win, move->img.img, 0, 0);
 	return (0);
 }
